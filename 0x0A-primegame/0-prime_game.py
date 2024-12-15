759fbb56feaa5ef112fca01cd2fbef838caa1c78
hussin-hegazy <hussinhegazy350@gmail.com>
2024-12-15 08:51:04 +0000

    Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+    
+    for i in range(2, int(limit ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, limit + 1, i):
+                is_prime[j] = False
+                
+    primes = [i for i in range(limit + 1) if is_prime[i]]
+    return primes
+
+def play_round(n, primes):
+    player_turn = "Maria"
+    numbers = list(range(1, n + 1))
+
+    while primes and primes[0] <= n:
+        prime = primes.pop(0)
+        numbers = [num for num in numbers if num % prime != 0]
+        player_turn = "Maria" if player_turn == "Ben" else "Ben"
+
+    winner = "Maria" if player_turn == "Ben" else "Ben"
+    return winner
+
+def isWinner(x, nums):
+    if x <= 0 or not nums:
+        return None
+
+    primes = sieve_of_eratosthenes(max(nums))
+    scores = {"Maria": 0, "Ben": 0}
+
+    for n in nums:
+        winner = play_round(n, primes[:])
+        scores[winner] += 1
+
+    if scores["Maria"] > scores["Ben"]:
+        return "Maria"
+    elif scores["Ben"] > scores["Maria"]:
+        return "Ben"
+    else:
+        return None
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,3 @@
+# Prime Game
+Maria and Ben play a game with prime numbers. This project determines the winner of the game for multiple rounds.
+
